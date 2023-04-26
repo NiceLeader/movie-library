d801befa8e40ca4dbb32a9e2ba85efce0b8eb9e5
NiceLeader <maciek2002.10@wp.pl>
2023-04-26 16:03:34 +0200

    Initial commit

---
+++ src/test/java/github/io/NiceLeader/movielibrary/MovieLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.NiceLeader.movielibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
