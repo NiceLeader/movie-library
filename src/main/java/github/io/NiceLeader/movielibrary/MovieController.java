210e9d116cac765e577ab7fea5ae848ee021838e
NiceLeader <maciek2002.10@wp.pl>
2023-05-08 17:15:42 +0200

    Mapping movies getAll() added

--- src/main/java/github/io/NiceLeader/movielibrary/MovieController.java
+++ src/main/java/github/io/NiceLeader/movielibrary/MovieController.java
@@ -1,11 +1,21 @@
 package github.io.NiceLeader.movielibrary;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 public class MovieController {
+    @Autowired
+    MovieRepository movieRepository;
 
     @GetMapping("/test")
     public int test() {
         return 1;
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getAll() {
+        return movieRepository.getAll();
     }
 }
---
+++ src/main/java/github/io/NiceLeader/movielibrary/MovieRepository.java
@@ -0,0 +1,18 @@
+package github.io.NiceLeader.movielibrary;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MovieRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    public List<Movie> getAll() {
+        return jdbcTemplate.query("SELECT id, name, rating FROM movie",
+                BeanPropertyRowMapper.newInstance(Movie.class));
+    }
+}
