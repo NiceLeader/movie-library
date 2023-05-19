3ef166762a8b5c8142c1709037556cb9cbd0b82b
NiceLeader <maciek2002.10@wp.pl>
2023-05-19 19:06:23 +0200

    properties

--- src/main/java/github/io/NiceLeader/movielibrary/MovieController.java
+++ src/main/java/github/io/NiceLeader/movielibrary/MovieController.java
@@ -23,4 +23,22 @@ public class MovieController {
     public void add(@RequestBody List<Movie> movies) {
         movieRepository.add(movies);
     }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int id) {
+        movieRepository.delete(id);
+    }
+
+    @PutMapping("")
+    public int update(@PathVariable("id")int id, @RequestBody Movie updatedMovie) {
+        Movie movie = movieRepository.getById(id);
+        if (movie != null){
+            movie.setName(updatedMovie.getName());
+            movie.setRating(updatedMovie.getRating());
+            movieRepository.update(movie);
+        }else {
+            return 0;
+        }
+        return id;
+    }
 }
--- src/main/java/github/io/NiceLeader/movielibrary/MovieRepository.java
+++ src/main/java/github/io/NiceLeader/movielibrary/MovieRepository.java
@@ -1,5 +1,6 @@
 package github.io.NiceLeader.movielibrary;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -8,12 +9,12 @@ import java.util.List;
 
 @Repository
 public class MovieRepository {
-    final
+    @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public MovieRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+
+
+
 
     public List<Movie> getAll() {
         return jdbcTemplate.query("SELECT id, name, rating FROM movielibrary.movie",
@@ -31,7 +32,13 @@ public class MovieRepository {
                 .update("INSERT INTO  movielibrary.movie(name, rating) VALUES (?, ?)",
                 movie.getName(), movie.getRating()
         ));
-
+    }
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM movielibrary.movie WHERE id = ?", id);
+    }
+    public void update(Movie movie) {
+        jdbcTemplate.update("UPDATE movielibrary.movie SET name = ?, rating = ? WHERE id = ?",
+                movie.getName(), movie.getRating(), movie.getId());
     }
 
 }
